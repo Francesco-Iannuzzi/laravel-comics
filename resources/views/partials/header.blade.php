7cc2eec5c2fcc76438da4de56021db4a14027208
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-05-23 18:31:17 +0200

    fix header style + add serach input

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,36 +7,39 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link py-4 {{ Route::currentRouteName() === 'characters' ? 'current_page text-light' : ''}}" href="{{ route('characters')}}">characters</a>
+                        <a class="nav-link fw-semibold py-4 {{ Route::currentRouteName() === 'characters' ? 'current_page text-light' : ''}}" href="{{ route('characters')}}">characters</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-4 {{ Route::currentRouteName() === 'comics' ? 'current_page text-light' : ''}}" href="{{ route('comics')}}">comics</a>
+                        <a class="nav-link fw-semibold py-4 {{ Route::currentRouteName() === 'comics' ? 'current_page text-light' : ''}}" href="{{ route('comics')}}">comics</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-4 {{ Route::currentRouteName() === 'movies' ? 'current_page text-light' : ''}}" href="{{ route('movies')}}">movies</a>
+                        <a class="nav-link fw-semibold py-4 {{ Route::currentRouteName() === 'movies' ? 'current_page text-light' : ''}}" href="{{ route('movies')}}">movies</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-4 {{ Route::currentRouteName() === 'tv' ? 'current_page text-light' : ''}}" href="{{ route('tv')}}">tv</a>
+                        <a class="nav-link fw-semibold py-4 {{ Route::currentRouteName() === 'tv' ? 'current_page text-light' : ''}}" href="{{ route('tv')}}">tv</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-4 {{ Route::currentRouteName() === 'games' ? 'current_page text-light' : ''}}" href="{{ route('games')}}">games</a>
+                        <a class="nav-link fw-semibold py-4 {{ Route::currentRouteName() === 'games' ? 'current_page text-light' : ''}}" href="{{ route('games')}}">games</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-4 {{ Route::currentRouteName() === 'collectibles' ? 'current_page text-light' : ''}}" href="{{ route('collectibles')}}">collectibles</a>
+                        <a class="nav-link fw-semibold py-4 {{ Route::currentRouteName() === 'collectibles' ? 'current_page text-light' : ''}}" href="{{ route('collectibles')}}">collectibles</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-4 {{ Route::currentRouteName() === 'videos' ? 'current_page text-light' : ''}}" href="{{ route('videos')}}">videos</a>
+                        <a class="nav-link fw-semibold py-4 {{ Route::currentRouteName() === 'videos' ? 'current_page text-light' : ''}}" href="{{ route('videos')}}">videos</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-4 {{ Route::currentRouteName() === 'fans' ? 'current_page text-light' : ''}}" href="{{ route('fans')}}">fans</a>
+                        <a class="nav-link fw-semibold py-4 {{ Route::currentRouteName() === 'fans' ? 'current_page text-light' : ''}}" href="{{ route('fans')}}">fans</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-4 {{ Route::currentRouteName() === 'news' ? 'current_page text-light' : ''}}" href="{{ route('news')}}">news</a>
+                        <a class="nav-link fw-semibold py-4 {{ Route::currentRouteName() === 'news' ? 'current_page text-light' : ''}}" href="{{ route('news')}}">news</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link py-4 {{ Route::currentRouteName() === 'shop' ? 'current_page text-light' : ''}}" href="{{ route('shop')}}">shop</a>
+                        <a class="nav-link fw-semibold py-4 {{ Route::currentRouteName() === 'shop' ? 'current_page text-light' : ''}}" href="{{ route('shop')}}">shop</a>
                     </li>
                 </ul>
+            </div>
+            <div>
+                <input type="search" name="search" class="border_dc border-0 ms-3 text-end" placeholder="Search"><i class="fa-solid fa-magnifying-glass"></i>
             </div>
         </nav>
     </div>
